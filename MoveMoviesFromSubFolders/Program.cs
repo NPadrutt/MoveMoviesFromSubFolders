d37660c60b8fd827f62929c8cc9db8fa8004d5c3
Nino Padrutt <nino.padrutt@apply-solutions.ch>
2016-02-17 17:33:56 +0100

    init comit

---
+++ MoveMoviesFromSubFolders/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.IO;
+using System.Linq;
+
+namespace MoveMoviesFromSubFolders
+{
+    class Program
+    {
+        const string DIRECTORY_NAME = @"C:\Users\ninop\Downloads";
+        static void Main(string[] args)
+        {
+            DirectoryInfo dirInfo = new DirectoryInfo(DIRECTORY_NAME);
+
+            if (!dirInfo.Exists) return;
+
+            var MyMusicFiles = Directory.GetFiles(DIRECTORY_NAME, "*.mkv", SearchOption.AllDirectories).ToList();
+
+            foreach (string file in MyMusicFiles)
+            {
+                FileInfo mFile = new FileInfo(file);
+                new FileInfo(file).MoveTo(Path.Combine(DIRECTORY_NAME, mFile.Name));
+            }
+        }
+    }
+}
