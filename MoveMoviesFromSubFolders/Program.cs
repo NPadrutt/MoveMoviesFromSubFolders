45f382f34f9a66782b88b600f75cf7359cd4b0cd
Nino Padrutt <nino.padrutt@apply-solutions.ch>
2019-07-06 11:42:58 +0200

    move to multimedia nas

--- MoveMoviesFromSubFolders/Program.cs
+++ MoveMoviesFromSubFolders/Program.cs
@@ -7,20 +7,21 @@ namespace MoveMoviesFromSubFolders
 {
     class Program
     {
+        private const string FOLDER_PATH = @"\\192.168.1.202\Media\Downloads";
         static void Main(string[] args)
         {
-            DirectoryInfo dirInfo = new DirectoryInfo(KnownFolders.Downloads.Path);
+            DirectoryInfo dirInfo = new DirectoryInfo(FOLDER_PATH);
 
             if (!dirInfo.Exists) return;
 
-            var myMovieFiles = Directory.GetFiles(KnownFolders.Downloads.Path, "*.mkv", SearchOption.AllDirectories).ToList();
+            var myMovieFiles = Directory.GetFiles(FOLDER_PATH, "*.mkv", SearchOption.AllDirectories).ToList();
 
             int filesMoved = 0;
             foreach (string file in myMovieFiles)
             {
                 FileInfo mFile = new FileInfo(file);
                 Console.WriteLine($"Move File: {mFile.Name}");
-                new FileInfo(file).MoveTo(Path.Combine(KnownFolders.Downloads.Path, mFile.Name));
+                new FileInfo(file).MoveTo(Path.Combine(FOLDER_PATH, mFile.Name));
                 filesMoved++;
             }
 
